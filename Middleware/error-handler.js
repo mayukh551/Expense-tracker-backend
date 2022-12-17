9ab4390485376bfa819456a1107956929112c78f
mayukh551 <may14ukh@gmail.com>
2022-12-17 19:35:30 +0530

    error-handling not working

--- Middleware/error-handler.js
+++ Middleware/error-handler.js
@@ -1,22 +1,8 @@
-function errorLogger(err, req, res, next) {
+function errorHandler(err, req, res, next) {
     console.log("error caught", err.type);
     console.log("Error is", err);
-    next(err);
-}
-
-function errorResponder(err, req, res, next) {
-    console.log("Sending appropriate msg to user")
     const { status = 500, message = 'Server Error' } = err;
     res.status(status).json({ error: err, isSuccess: false, message: message })
-    next(err);
 }
 
-function failSafeHandler(err, req, res, next) {
-    res.status(500).json({ error: err, isSuccess: false, message: "Server Error" })
-}
-
-module.exports = {
-    errorLogger,
-    errorResponder,
-    failSafeHandler
-}
+module.exports = errorHandler;
