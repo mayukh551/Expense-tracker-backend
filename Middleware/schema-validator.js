28e48c8aecf38852326d71cd8a03fffabed90db8
mayukh551 <may14ukh@gmail.com>
2023-03-05 13:09:07 +0530

    Validation error directly sent to error-middleware

--- Middleware/schema-validator.js
+++ Middleware/schema-validator.js
@@ -1,10 +1,10 @@
 const Joi = require('joi');
 const JoiObjectId = require('joi-objectid')(Joi);
-const CrudError = require('../Error/CrudError');
+// const CrudError = require('../Error/CrudError');
 
 function validateSchema(schema, body, next) {
     const { error } = schema.validate(body);
-    if (error) next(new CrudError(error.name));
+    if (error) next(error);
     else next();
 }
 
