fd5576240a48afc5e8aa936ccd2867b582ef6a14
mayukh551 <may14ukh@gmail.com>
2023-07-15 23:22:55 +0530

    CI-CD for deployment on GKE added

--- k8s/autocannon.js
+++ k8s/autocannon.js
@@ -5,7 +5,8 @@ const autocannon = require('autocannon');
 // async/await
 async function foo() {
     const result = await autocannon({
-        url: 'http://localhost:31980/',
+        // url: 'http://localhost:31980/',
+        url: 'http://34.100.191.95:5000/',
         connections: 10, //default
         pipelining: 1, // default
         duration: 300 // default
